1071c99b4956f7bbba7482f7efb0c8eb4401c8ea
=William Roberts <=>
2023-07-17 13:09:09 +0100

    r

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" style={{overflowX:"hidden"}}>
       <body className={lato.className}>
         <ProviderForTheme>
         <AuthProvider>
