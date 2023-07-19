0c812c1121a23cdce38a5ce14292065b9e79f3a0
=William Roberts <=>
2023-07-19 18:27:36 +0100

    r

--- app/signIn/components/signInForm.tsx
+++ app/signIn/components/signInForm.tsx
@@ -9,8 +9,11 @@ import NotificationPortal from '../../signUp/components/notificationPortal'
 
 const SignInForm = () => {
   const {signIn}=useAuth()
+  const demoUserEmail = "demo123@gmail.com"
+  const demoUserPass = "padding1rem"
   const {setNotification,setOpenNotification,openNotification}=useNotifications()
     const [state,dispatch]=useReducer(reducer,{email:"",password:""})
+    const [isCopied,setIsCopied]=useState({email:false,password:false})
     const handleSubmit = async (e)=>{
         e.preventDefault()
         //console.log(state.email,state.password)
@@ -32,16 +35,41 @@ const SignInForm = () => {
         }
        
     }
+    const handleCopy = (e)=>{
+      if (e.target.textContent.includes("email")){
+        navigator.clipboard.writeText(demoUserEmail)
+        dispatch({type:"email",value:demoUserEmail})
+      }else if (e.target.textContent.includes("password")){
+        navigator.clipboard.writeText(demoUserPass)
+        dispatch({type:"password",value:demoUserPass})
+      }
+     
+
+    setTimeout(()=>{
+      setIsCopied((prev)=>({email:false,password:false}))
+    },2000)
+    }
   return (
     <form onSubmit={handleSubmit} className='auth__form'>
         <InputReusable type='email' placeholder='Email address'
         required={true} name='input-email' value={state.email}
         handleChange={(e)=>handleInputChange("email",e)}
         />
+        <span 
+        onClick={(e)=>handleCopy(e)}
+        className='text-xs 
+        opacity-60 hover:opacity-100
+        cursor-pointer'>Demo email: demo123@gmail.com <b>🗐</b></span>
         <InputReusable type='password' placeholder='Password'
         required={true} name='input-password' value={state.password}
         handleChange={(e)=>handleInputChange("password",e)}
         />
+        <span 
+        
+        onClick={handleCopy}
+        className='text-xs opacity-60
+        hover:opacity-100
+        cursor-pointer'>Demo password: padding1rem  <b>🗐</b></span>
         <AuthButton text={"Sign in"} 
         type={"submit"}
         disabled={false}/>
