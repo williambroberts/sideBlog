b2c7b824f2c5fee5a73b97d6791c6ed5da33d356
= <=>
2023-08-16 08:51:04 +0100

    nav fix

--- components/home/MostViewedBlogs.tsx
+++ components/home/MostViewedBlogs.tsx
@@ -10,7 +10,7 @@ interface theProps {
 }
 const MostViewedBlogs = ({}:theProps) => {
   function getDevice(){
-    return !(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent));
+    return !(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator?.userAgent));
 }
 const desktop = getDevice()
 
