de67f69dec00825173ad94a6858f3d68cfae1cd0
= <=>
2023-08-26 07:49:34 +0100

    Rr2

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -1,12 +1,15 @@
+import Image from 'next/image'
 import React from 'react'
-
+import img from "../../public/images/android-chrome-384x384.png"
 const FooterHorizontal = () => {
   return (
     <footer className='bg-[var(--bg-3)] flex flex-row
     items-center justify-between 
     '>
       <span></span>
-        <span>⚛</span>
+        <span className='footer__mid'>
+          <Image src={img} alt='' width={24} height={24}/> 
+          </span>
         <span></span>
     </footer>
   )
